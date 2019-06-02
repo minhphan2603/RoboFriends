13f4475272fad653d8e9f0ae9479207ad71ec3c1
minhphan2603 <48484833+minhphan2603@users.noreply.github.com>
2019-06-03 00:02:57 +0700

    Updates

---
+++ precache-manifest.2c0ab88a22d0542699c2f37455b34658.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d48d68f29921c5c588da",
+    "url": "/RoboFriends/static/js/runtime~main.19bee101.js"
+  },
+  {
+    "revision": "516faad99eb001f3e833",
+    "url": "/RoboFriends/static/js/main.f9b941b5.chunk.js"
+  },
+  {
+    "revision": "13d7a69cb03b2df28a3e",
+    "url": "/RoboFriends/static/js/2.521fcfff.chunk.js"
+  },
+  {
+    "revision": "516faad99eb001f3e833",
+    "url": "/RoboFriends/static/css/main.b873ae5a.chunk.css"
+  },
+  {
+    "revision": "13d7a69cb03b2df28a3e",
+    "url": "/RoboFriends/static/css/2.952c8838.chunk.css"
+  },
+  {
+    "revision": "f6000b430248a1ed23c095f4b6992bac",
+    "url": "/RoboFriends/index.html"
+  }
+];
