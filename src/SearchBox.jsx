4ca8f48251aa3a39ff680126305180e422becd68
minhphan2603 <48484833+minhphan2603@users.noreply.github.com>
2019-06-03 00:07:10 +0700

    edit searchbox

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -4,9 +4,6 @@ const SearchBox = ({searchChange , searchField}) => {
     const x = 1;
     return (
         <div>
-            {x === 1 ? <p>tao la 1</p> : <p>tao khac 1</p>}
-            <p>{x === 1 ? 'tao la 1' : 'tao khac 1'}</p>
-            <p>'{searchField}'</p>
             <input className="pa3 ba b--green bg-lightest-blue" 
             type="search" 
             placeholder="search robots"
